8fbbd784b8ef38f6eb488ab2468ef8097b816357
Tai Taio <robotaitai@gmail.com>
2020-03-25 15:45:10 +0200

    6

--- can/io/parser.py
+++ can/io/parser.py
@@ -117,9 +117,10 @@ class Parser(BaseIOHandler, Listener):
 
             if arbitration_id_string == "0610":
                 list620 = data_string.split()
-                print("1b: ",list620[1],", 1a: ",hex(int(list620[1],16)-0x10))
-                print("3b: ",list620[3],", 3a: ",hex(int(list620[3],16)-0x10))
-                print("5b: ",list620[5],", 5a: ",hex(int(list620[5],16)-0x10))
+                # print("1b: ",list620[1],", 1a: ",hex(int(list620[1],16)-0x10))
+                # print("3b: ",list620[3],", 3a: ",hex(int(list620[3],16)-0x10))
+                # print("5b: ",list620[5],", 5a: ",hex(int(list620[5],16)-0x10))
+                print(int(list620[1],2))
 
 
 
